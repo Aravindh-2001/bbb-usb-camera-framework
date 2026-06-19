97f5c444234aafbeaa139170fbd1f2f59f9e8563
Aravindh Selvaraj <aravindhselvaraj47@gmail.com>
2026-06-19 08:46:34 +0000

    Add camera device discovery check

--- app/src/main.c
+++ app/src/main.c
@@ -1,11 +1,20 @@
+#include <sys/stat.h>
+#include <unistd.h>
 #include <stdio.h>
 #include "camera.h"
 
 int main()
 {
+    struct stat st;
     int ret;
 
     printf("USB Camera Framework Started\n");
+
+    if (stat("/dev/video0", &st) != 0)
+    {
+        printf("No camera device found at /dev/video0\n");
+        return -1;
+    }
 
     ret = camera_open("/dev/video0");
 
